5eb2e09db9112c5de44b02ebddb9f3cdc0c87a45
xuchengli <lixucheng@aliyun.com>
2023-12-21 15:12:50 +0800

    try chat member count

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+(async() => {
+  const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-bot.on('message', msg => {
-  if (msg.new_chat_members) {
-    console.log('新加入的用户: ', msg.new_chat_members);
-  }
-});
+  const count = await bot.getChatMemberCount(process.env.CHAT_ID);
+  console.log('现有用户总数: ', count);
+
+  bot.on('message', msg => {
+    if (msg.new_chat_members) {
+      console.log('新加入的用户: ', msg.new_chat_members);
+    }
+  });
+})();
